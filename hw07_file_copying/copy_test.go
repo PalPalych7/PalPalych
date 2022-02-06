3b1e857e0f252fb2ec8d915b04872a15b57776b8
PalPalych7 <PalPalych@bk.ru>
2022-02-06 22:46:15 +0300

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	// исходный файл не существует
+	myEr := Copy("1.txt", "2.txt", 0, 0)
+	require.Error(t, myEr)
+
+	// offset<0
+	myEr = Copy("testdata/input.txt", "2.txt", -2, 0)
+	require.Error(t, myEr)
+	require.Truef(t, errors.Is(myEr, ErrOffsetLes0), "actual error %q", myEr)
+
+	// limit<0
+	myEr = Copy("testdata/input.txt", "2.txt", 0, -10)
+	require.Error(t, myEr)
+	require.Truef(t, errors.Is(myEr, ErrLimitLes0), "actual error %q", myEr)
+
+	// ofset > len
+	myEr = Copy("testdata/input.txt", "2.txt", 10000, 0)
+	require.Error(t, myEr)
+	require.Truef(t, errors.Is(myEr, ErrOffsetExceedsFileSize), "actual error %q", myEr)
+	return
 }
