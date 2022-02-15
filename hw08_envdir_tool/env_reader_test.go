d7bdd062e47d8044040adc9b4004a4e82101f898
PalPalych7 <PalPalych@bk.ru>
2022-02-16 00:33:52 +0300

    HW08-3

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestReadDir(t *testing.T) {
-	/*	_, myEr := ReadDir("bad dir")
-		require.Error(t, myEr)
-		require.Truef(t, errors.Is(myEr, ErrBadDir), "actual error %q", myEr)
-	*/
+	_, myEr := ReadDir("bad dir")
+	require.Error(t, myEr)
+	require.Truef(t, errors.Is(myEr, ErrBadDir), "actual error %q", myEr)
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestRunCmd(t *testing.T) {
-	/*	myMap := make(Environment)
-		parmList := []string{"/test.sh", "parm1"}
-		myRes := RunCmd(parmList, myMap)
-		require.Equal(t, -2, myRes)
+	myMap := make(Environment)
+	parmList := []string{"/test.sh", "parm1"}
+	myRes := RunCmd(parmList, myMap)
+	require.Equal(t, -2, myRes)
 
-		parmList = []string{}
-		myMap["key1"] = EnvValue{"val1", true}
-		myRes = RunCmd(parmList, myMap)
-		require.Equal(t, -1, myRes)*/
+	parmList = []string{}
+	myMap["key1"] = EnvValue{"val1", true}
+	myRes = RunCmd(parmList, myMap)
+	require.Equal(t, -1, myRes)
 }
